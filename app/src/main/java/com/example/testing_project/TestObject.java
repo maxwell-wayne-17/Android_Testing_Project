6724f198aa10bfa6114c0a74a22343c8c3e462cf
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2021-11-29 21:24:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/testing_project/TestObject.java
@@ -0,0 +1,54 @@
+package com.example.testing_project;
+
+import java.util.Objects;
+
+public class TestObject {
+
+    // Constants
+    private static int DEFAULT_NUM = 0;
+    private static String DEFAULT_STRING = "Default";
+
+    private int testNum;
+    private String testString;
+    private boolean testBool;
+
+    // Default constructor
+    public TestObject(){
+        testNum = DEFAULT_NUM;
+        testString = DEFAULT_STRING;
+        testBool = false;
+    }
+
+    public TestObject(int num, String str, boolean bool){
+        testNum = num;
+        testString = str;
+        testBool = bool;
+    }
+
+    public void increment(){ testNum++; }
+
+    public void decrement(){ testNum--; }
+
+    public int getNum(){ return testNum; }
+
+    public void setStr(String str){ testString = str; }
+
+    public String getStr(){ return testString; }
+
+    public void switchBool(){ testBool = !testBool; }
+
+    public boolean getBool(){ return testBool; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestObject that = (TestObject) o;
+        return testNum == that.testNum && testBool == that.testBool && Objects.equals(testString, that.testString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(testNum, testString, testBool);
+    }
+}
