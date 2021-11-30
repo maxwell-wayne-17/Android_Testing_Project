6dce7e3c59c2e68d56a34dea3040b396e7e849be
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2021-11-29 22:03:42 -0500

    equals unit tests working

--- app/src/test/java/com/example/testing_project/ExampleUnitTest.java
+++ app/src/test/java/com/example/testing_project/ExampleUnitTest.java
@@ -14,4 +14,73 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    ///// Everything above this was added by default with empty project /////
+
+    ///// Test equals method /////
+    @Test
+    public void test_testObjectDefault_equalsTrue(){
+        TestObject one = new TestObject();
+        TestObject two = new TestObject();
+
+        boolean expected = true;
+        // Test objects equal themselves and each other
+        boolean actual = one.equals(two) && two.equals(one) && one.equals(one) && two.equals(two);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_testObjectExplicit_equalsTrue(){
+        TestObject one = new TestObject(10, "test", true);
+        TestObject two = new TestObject(10, "test", true);
+
+        // Test objects equal themselves and each other
+        boolean actual = one.equals(two) && two.equals(one) && one.equals(one) && two.equals(two);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void test_testObjectNum_equalsFalse(){
+        TestObject one = new TestObject(0, "test", true);
+        TestObject two = new TestObject(10, "test", true);
+
+        // Test objects do not equal each other
+        boolean actual = one.equals(two) || two.equals(one);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void test_testObjectStr_equalsFalse(){
+        TestObject one = new TestObject(0, "one", true);
+        TestObject two = new TestObject(0, "two", true);
+
+        // Test objects do not equal each other
+        boolean actual = one.equals(two) || two.equals(one);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void test_testObjectBool_equalsFalse(){
+        TestObject one = new TestObject(0, "test", true);
+        TestObject two = new TestObject(0, "test", false);
+
+        // Test objects do not equal each other
+        boolean actual = one.equals(two) || two.equals(one);
+
+        assertFalse(actual);
+    }
+
+    //// Test inc/dec methods /////
+    @Test
+    public void test_testObject_default_constructor(){
+
+        TestObject explicit = new TestObject(0, "Default", false);
+        TestObject def = new TestObject();
+
+
+    }
 }
